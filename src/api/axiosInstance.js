8d426d1714b645082c05a5ef4670eed5f697ba8b
YugankDabas <142775559+YugankDabas@users.noreply.github.com>
2026-02-24 21:43:47 +0530

    vercel_error

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const rawBaseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000';
+baseURL: import.meta.env.VITE_API_BASE_URL 
+  || (import.meta.env.DEV 
+        ? 'http://localhost:8000' 
+        : 'https://analytics-dashboard-backend-5kz6.onrender.com')
 const api = axios.create({
   baseURL: rawBaseURL.endsWith('/') ? rawBaseURL.slice(0, -1) : rawBaseURL,
 });
